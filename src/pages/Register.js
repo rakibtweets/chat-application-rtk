7d6566c7d1d1596031d08b7905246a9fab59c15d
Rakib Hasan <rakibhasanroky0@gmail.com>
2023-01-23 23:38:46 +0600

    register user

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,122 +1,173 @@
-import { Link } from "react-router-dom";
-import logoImage from "../assets/images/lws-logo-light.svg";
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import logoImage from '../assets/images/lws-logo-light.svg';
+import Error from '../components/ui/Error';
+import { useRegisterMutation } from '../features/auth/authApi';
 
 export default function Register() {
-    return (
-        <div className="grid place-items-center h-screen bg-[#F9FAFB">
-            <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-                <div className="max-w-md w-full space-y-8">
-                    <div>
-                        <Link to="/">
-                            <img
-                                className="mx-auto h-12 w-auto"
-                                src={logoImage}
-                                alt="Learn with sumit"
-                            />
-                        </Link>
-                        <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-                            Create your account
-                        </h2>
-                    </div>
-                    <form className="mt-8 space-y-6" action="#" method="POST">
-                        <input type="hidden" name="remember" value="true" />
-                        <div className="rounded-md shadow-sm -space-y-px">
-                            <div>
-                                <label htmlFor="name" className="sr-only">
-                                    Full Name
-                                </label>
-                                <input
-                                    id="name"
-                                    name="Name"
-                                    type="Name"
-                                    autoComplete="Name"
-                                    required
-                                    className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-violet-500 focus:border-violet-500 focus:z-10 sm:text-sm"
-                                    placeholder="Name"
-                                />
-                            </div>
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [agreed, setAgreed] = useState(false);
+  const [error, setError] = useState('');
+  console.log('env url:', process.env.REACT_APP_API_URL);
 
-                            <div>
-                                <label
-                                    htmlFor="email-address"
-                                    className="sr-only"
-                                >
-                                    Email address
-                                </label>
-                                <input
-                                    id="email-address"
-                                    name="email"
-                                    type="email"
-                                    autoComplete="email"
-                                    required
-                                    className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-violet-500 focus:border-violet-500 focus:z-10 sm:text-sm"
-                                    placeholder="Email address"
-                                />
-                            </div>
+  const [register, { data: user, isLoading, error: resError }] =
+    useRegisterMutation();
 
-                            <div>
-                                <label htmlFor="password" className="sr-only">
-                                    Password
-                                </label>
-                                <input
-                                    id="password"
-                                    name="password"
-                                    type="password"
-                                    autoComplete="current-password"
-                                    required
-                                    className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-violet-500 focus:border-violet-500 focus:z-10 sm:text-sm"
-                                    placeholder="Password"
-                                />
-                            </div>
+  const reset = () => {
+    setName('');
+    setEmail('');
+    setPassword('');
+    setConfirmPassword('');
+  };
 
-                            <div>
-                                <label
-                                    htmlFor="confirmPassword"
-                                    className="sr-only"
-                                >
-                                    Confirm Password
-                                </label>
-                                <input
-                                    id="confirmPassword"
-                                    name="confirmPassword"
-                                    type="confirmPassword"
-                                    autoComplete="current-confirmPassword"
-                                    required
-                                    className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-violet-500 focus:border-violet-500 focus:z-10 sm:text-sm"
-                                    placeholder="confirmPassword"
-                                />
-                            </div>
-                        </div>
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setError('');
 
-                        <div className="flex items-center justify-between">
-                            <div className="flex items-center">
-                                <input
-                                    id="remember-me"
-                                    name="remember-me"
-                                    type="checkbox"
-                                    className="h-4 w-4 text-violet-600 focus:ring-violet-500 border-gray-300 rounded"
-                                />
-                                <label
-                                    htmlFor="accept-terms"
-                                    className="ml-2 block text-sm text-gray-900"
-                                >
-                                    Agreed with the terms and condition
-                                </label>
-                            </div>
-                        </div>
+    if (confirmPassword !== password) {
+      setError('Password does not match');
+    } else {
+      register({
+        name,
+        email,
+        password
+      });
+    }
 
-                        <div>
-                            <button
-                                type="submit"
-                                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-violet-600 hover:bg-violet-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-violet-500"
-                            >
-                                <span className="absolute left-0 inset-y-0 flex items-center pl-3"></span>
-                                Sign up
-                            </button>
-                        </div>
-                    </form>
-                </div>
+    reset();
+  };
+
+  useEffect(() => {
+    console.log(user);
+    console.log(resError);
+  }, [user, resError]);
+
+  return (
+    <div className="grid place-items-center h-screen bg-[#F9FAFB">
+      <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+        <div className="max-w-md w-full space-y-8">
+          <div>
+            <Link to="/">
+              <img
+                className="mx-auto h-12 w-auto"
+                src={logoImage}
+                alt="Learn with sumit"
+              />
+            </Link>
+            <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+              Create your account
+            </h2>
+          </div>
+          <form onSubmit={handleSubmit} className="mt-8 space-y-6">
+            {/* <input type="hidden" name="remember" value="true" /> */}
+            <div className="rounded-md shadow-sm -space-y-px">
+              <div>
+                <label htmlFor="name" className="sr-only">
+                  Full Name
+                </label>
+                <input
+                  id="name"
+                  name="Name"
+                  type="Name"
+                  autoComplete="Name"
+                  required
+                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-violet-500 focus:border-violet-500 focus:z-10 sm:text-sm"
+                  placeholder="Name"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                />
+              </div>
+
+              <div>
+                <label htmlFor="email-address" className="sr-only">
+                  Email address
+                </label>
+                <input
+                  id="email-address"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  required
+                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-violet-500 focus:border-violet-500 focus:z-10 sm:text-sm"
+                  placeholder="Email address"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
+
+              <div>
+                <label htmlFor="password" className="sr-only">
+                  Password
+                </label>
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
+                  required
+                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-violet-500 focus:border-violet-500 focus:z-10 sm:text-sm"
+                  placeholder="Password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+
+              <div>
+                <label htmlFor="confirmPassword" className="sr-only">
+                  Confirm Password
+                </label>
+                <input
+                  id="confirmPassword"
+                  name="confirmPassword"
+                  type="confirmPassword"
+                  autoComplete="current-confirmPassword"
+                  required
+                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-violet-500 focus:border-violet-500 focus:z-10 sm:text-sm"
+                  placeholder="confirmPassword"
+                  value={confirmPassword}
+                  onChange={(e) => setConfirmPassword(e.target.value)}
+                />
+              </div>
             </div>
+
+            <div className="flex items-center justify-between">
+              <div className="flex items-center">
+                <input
+                  id="agree"
+                  name="agree"
+                  type="checkbox"
+                  checked={agreed}
+                  required
+                  onChange={(e) => setAgreed(e.target.checked)}
+                  className="h-4 w-4 text-violet-600 focus:ring-violet-500 border-gray-300 rounded"
+                />
+                <label
+                  htmlFor="accept-terms"
+                  className="ml-2 block text-sm text-gray-900"
+                >
+                  Agreed with the terms and condition
+                </label>
+              </div>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                disabled={isLoading}
+                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-violet-600 hover:bg-violet-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-violet-500"
+              >
+                <span className="absolute left-0 inset-y-0 flex items-center pl-3"></span>
+                Sign up
+              </button>
+            </div>
+
+            {error !== '' && <Error message={error} />}
+          </form>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
