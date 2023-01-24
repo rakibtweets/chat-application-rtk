bf6470fb4b217fdb5d05080f73dd7a5778e03558
Rakib Hasan <rakibhasanroky0@gmail.com>
2023-01-24 17:27:56 +0600

    logOut feature

--- src/components/inbox/Navigation.js
+++ src/components/inbox/Navigation.js
@@ -1,25 +1,32 @@
-import { Link } from "react-router-dom";
-import logoImage from "../../assets/images/lws-logo-dark.svg";
+import { useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
+import logoImage from '../../assets/images/lws-logo-dark.svg';
+import { userLoggedOut } from '../../features/auth/authSlice';
 
 export default function Navigation() {
-    return (
-        <nav className="border-general sticky top-0 z-40 border-b bg-violet-700 transition-colors">
-            <div className="max-w-7xl mx-auto">
-                <div className="flex justify-between h-16 items-center">
-                    <Link to="/">
-                        <img
-                            className="h-10"
-                            src={logoImage}
-                            alt="Learn with Sumit"
-                        />
-                    </Link>
-                    <ul>
-                        <li className="text-white">
-                            <a href="#">Logout</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-    );
+  const dispatch = useDispatch();
+
+  const handleLogOut = () => {
+    dispatch(userLoggedOut());
+    localStorage.clear();
+  };
+
+  return (
+    <nav className="border-general sticky top-0 z-40 border-b bg-violet-700 transition-colors">
+      <div className="max-w-7xl mx-auto">
+        <div className="flex justify-between h-16 items-center">
+          <Link to="/">
+            <img className="h-10" src={logoImage} alt="Learn with Sumit" />
+          </Link>
+          <ul>
+            <li className="text-white">
+              <button className="cursor-pointer" onClick={handleLogOut}>
+                Logout
+              </button>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 }
